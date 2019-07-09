7c8df0bad6dabf0e4ce458d2c39279bbf9da7537
mallik arjun <a.malli1201@gmail.com>
2019-07-09 12:44:22 +0530

    sample app

---
+++ src/main/java/Jenkins_Demo/jenkinsDemo/App.java
@@ -0,0 +1,13 @@
+package Jenkins_Demo.jenkinsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
